9133431663ec5d1d76046959715c2fb7eea99489
hoanpc <hoanpc1304@gmail.com>
2026-03-18 09:44:52 +0700

    unit test ci cd

---
+++ CI_CDTests/CI_CDTests.swift
@@ -0,0 +1,17 @@
+//
+//  CI_CDTests.swift
+//  CI_CDTests
+//
+//  Created by HoanPC on 18/3/26.
+//
+
+import Testing
+import XCTest
+
+struct CI_CDTests {
+
+    @Test func example() async throws {
+        XCTAssertEqual(1 + 1, 2)
+    }
+
+}
